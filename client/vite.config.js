068168b833022a708ca7a4a820a956f223782db0
gron8a-pixel <gron8a-pixel@example.com>
2025-10-07 05:45:25 +0300

    Исправлена сборка для Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  // ...
   build: {
-    outDir: '../server/public',
-    emptyOutDir: true
-  },
-  base: './'
+    outDir: '../server/public'
+  }
 });
